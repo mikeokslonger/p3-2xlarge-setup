bb426808690356bc3c638f17303e52abbec1509b
EC2 Default User <ec2-user@ip-172-31-19-206.eu-west-1.compute.internal>
2018-05-28 15:29:46 +0000

    Changing installation order

--- init_dl_ami.sh
+++ init_dl_ami.sh
@@ -14,17 +14,17 @@ echo '{
 }' > $HOME/.jupyter/jupyter_notebook_config.json
 
 
-echo "Installing Packages"
-source activate pytorch_p36
-conda install -y fastparquet pyarrow python-snappy jupyterlab nodejs -c conda-forge
-pip install tqdm s3fs
-
-echo "Installing plugins"
+echo "Fixing jupyter"
+conda install -y jupyter jupyterlab nodejs -c conda-forge
 mkdir -p $(jupyter --data-dir)/nbextensions
 cd $(jupyter --data-dir)/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
 jupyter nbextension enable vim_binding/vim_binding
 jupyter labextension install @jupyterlab/plotly-extension
+
+source activate pytorch_p36
+conda install -y fastparquet pyarrow python-snappy jupyterlab nodejs -c conda-forge
+pip install tqdm s3fs
 
 echo "Starting Jupyter in tmux session"
 tmux new-session -d -s "jupyter" "bash -c 'jupyter notebook --ip 0.0.0.0 --certfile mycert.pem --keyfile mykey.key'"
